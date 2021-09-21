0184b583f6f28e2944bd9e2dda36ebe5f0d65148
pcgr <pcgr89@gmail.com>
2021-09-20 21:39:09 -0300

    ​deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd​ dist​
 git init
 git add -A
 git commit -m ​'deploy'
-git push -f git@github.com:pcgr89/deploytest.git master:gh-pages​​
+git push -f https://github.com/pcgr89/deploytest.git master:gh-pages​​
 cd​ -
