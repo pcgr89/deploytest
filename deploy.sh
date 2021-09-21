c5fb0541a34a80ef28a19e2d4ecaf34508001105
pcgr <pcgr89@gmail.com>
2021-09-20 21:34:03 -0300

    ​deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@ npm run build​
 cd​ dist​
 git init
 git add -A
-git commit -m ​'deploy'​​
-# si los cambios se suben a https://<USERNAME>.github.io/ ejecutar lo siguiente
-git push -f git@github.com:pcgr89/deploytest.git master​ read​ -p ​"Cambios actualizados en Bitbucket. Presione [Enter] para continuar..."​​
-# si los cambios se suben a https://<USERNAME>.github.io/<REPO> ejecutar lo siguiente
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages​​
+git commit -m ​'deploy'
+# git push -f git@github.com:pcgr89/deploytest.git master​ read​ -p ​"Cambios actualizados en Bitbucket. Presione [Enter] para continuar..."​​
+
+git push -f git@github.com:pcgr89/deploytest.git master:gh-pages​​
 cd​ -
