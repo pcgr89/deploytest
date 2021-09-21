638f7988565657976fd739501df9c7648ee267b2
pcgr <pcgr89@gmail.com>
2021-09-20 21:35:19 -0300

    ​deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
-
 set​ -e​
 npm run build​
 cd​ dist​
 git init
 git add -A
 git commit -m ​'deploy'
-# git push -f git@github.com:pcgr89/deploytest.git master​ read​ -p ​"Cambios actualizados en Bitbucket. Presione [Enter] para continuar..."​​
-
 git push -f git@github.com:pcgr89/deploytest.git master:gh-pages​​
 cd​ -
