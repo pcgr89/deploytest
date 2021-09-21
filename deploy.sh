d829981703c010b5761e5ca9ba9638a809d91ce3
pcgr <pcgr89@gmail.com>
2021-09-20 21:32:36 -0300

    ​deploy​​

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
-# abort on errors
+
 set​ -e​
-# build
 npm run build​
-# navigate into the build output directory
 cd​ dist​
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME​​
 git init
 git add -A
 git commit -m ​'deploy'​​
